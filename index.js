d8f1dec489d4965660627d3ed234e6789cc97444
Ray <rainiery10457@gmail.com>
2023-06-12 15:25:31 -0400

    fixed index.js and style.css

--- index.js
+++ index.js
@@ -5,53 +5,59 @@ const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
 
 search.addEventListener('click', () => {
+
     const APIKey = '3996f8ca96c42c085999cea7be7aa494';
     const city = document.querySelector('.search-box input').value;
 
     if (city === '')
-    return;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
-        if (json.cod === '404') {
-            container.style.height = '400px';
-            weatherBox.style.display= 'none';
-            weatherDetails.style.display = 'none';
-            error404.style.display = 'block';
-            error404.classList.add('fadeIn');
-            return;
-        }
+        return;
 
-        error404.style.display = 'none';
-        error404.classList.remove('fadeIn');
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+        .then(response => response.json())
+        .then(json => {
 
-        const image = document.querySelector('.weather-box img');
-        const temperature = document.querySelector('.weather-box .temperature');
-        const description = document.querySelector('.weather-box .description');
-        const humidity = document.querySelector('weather-details .humidity span');
-        const wind = document.querySelector('.weather-details .wind span');
+            if (json.cod === '404') {
+                container.style.height = '400px';
+                weatherBox.style.display = 'none';
+                weatherDetails.style.display = 'none';
+                error404.style.display = 'block';
+                error404.classList.add('fadeIn');
+                return;
+            }
 
-        switch (json.weather[0].main){
-            case 'Clear':
-                image.src = 'images/clear.png';
-                break;
+            error404.style.display = 'none';
+            error404.classList.remove('fadeIn');
 
-            case 'Rain':
-                image.src = 'images/rain.png';
-                break;
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+            const humidity = document.querySelector('.weather-details .humidity span');
+            const wind = document.querySelector('.weather-details .wind span');
 
-            case 'Snow':
-                image.src = 'images/snow.png';
-                break;
+            switch (json.weather[0].main) {
+                case 'Clear':
+                    image.src = 'images/clear.png';
+                    break;
 
-            case 'clouds':
-                image.src = 'images/snow.png';
-                break;
+                case 'Rain':
+                    image.src = 'images/rain.png';
+                    break;
 
-            case 'Haze':
-                image.src = 'images/cloud.png';
-                break;
-            default:
-                image.src = '';
-        }
+                case 'Snow':
+                    image.src = 'images/snow.png';
+                    break;
+
+                case 'Clouds':
+                    image.src = 'images/cloud.png';
+                    break;
+
+                case 'Haze':
+                    image.src = 'images/mist.png';
+                    break;
+
+                default:
+                    image.src = '';
+            }
 
             temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
             description.innerHTML = `${json.weather[0].description}`;
@@ -64,5 +70,8 @@ search.addEventListener('click', () => {
             weatherDetails.classList.add('fadeIn');
             container.style.height = '590px';
 
-    });
+
+        });
+
+
 });
